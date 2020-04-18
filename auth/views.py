96577276eafb108de477f0da45b6351187cba076
Jatin Katyal <jatin.katyal13@gmail.com>
2020-04-18 14:03:34 +0530

    signup functionality added

--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,9 @@
+from django.views import View
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+from auth import forms
 
 # Create your views here.
 class Login(LoginView):
@@ -6,3 +11,22 @@ class Login(LoginView):
 
 class Logout(LogoutView):
   pass
+
+class Signup(View):
+  def get(self, request):
+    context = {
+      "form": forms.SignUpForm()
+    }
+    return render(request, 'auth/signup.html', context)
+  
+  def post(self, request):
+    form = forms.SignUpForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('/')
+    
+    context = {
+      "form": form
+    }
+    return render(request, 'auth/signup.html', context)
--- social/views.py
+++ social/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from social import models
 
 # Create your views here.
-class Wall(ListView):
+class Wall(LoginRequiredMixin, ListView):
   queryset = models.Post.objects.all()
   context_object_name = 'posts'
   template_name = 'social/wall.html'
+  login_url = 'auth/login'
 
